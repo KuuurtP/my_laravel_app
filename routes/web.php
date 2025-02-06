b88d1d416429d9aad0fe51baea6f99f4dab1de34
KuuurtP <kurt.poklay@gmail.com>
2025-02-06 21:04:27 +0800

    updated lara

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController; // Import the controller
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('notes', NoteController::class); // Add the resource route
